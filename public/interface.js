6a62e914ab96b2149391d31004835587ea167556
Craig Horsborough <craighorsborough@gmail.com>
2015-03-04 22:01:00 +0000

    Restructured & added server

---
+++ public/interface.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+	var thermostat = new Thermostat;
+
+	$.get('http://www.myweather2.com/developer/forecast.ashx?uac=UuJ5tIZAKz&output=json&query=SW1', function(data) {
+		$('.outside_temp h2').text(data['weather']['curren_weather'][0]['temp'])
+			console.log(data);
+	});
+
+	$('.increase_temp').on('click', function(){
+		$('.thermostat h1').text(thermostat.increaseTemperature());
+	});
+
+	$('.decrease_temp').on('click', function(){
+		$('.thermostat h1').text(thermostat.decreaseTemperature());
+	});
+
+	$('.reset_temp').on('click', function(){
+		$('.thermostat h1').text(thermostat.resetTemperature());
+	});
+
+	$('.power_saver_on').on('click', function(){
+		$('.thermostat h2').text(thermostat.powerSaverOn());
+		$('.thermostat h1').text(thermostat.temperature);
+	});
+
+	$('.power_saver_off').on('click', function(){
+		$('.thermostat h2').text(thermostat.powerSaverOff());
+	});
+
+});
